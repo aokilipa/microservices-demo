425f3d2a062aa42595f1bc244012c5f1d0559894
aokilipa <aokilipa@ltmactech008.local>
2021-06-11 20:42:52 +0300

    add spring cloud eureka client to user service

--- user-ms/src/main/java/com/example/user/UserMsApplication.java
+++ user-ms/src/main/java/com/example/user/UserMsApplication.java
@@ -1,6 +1,5 @@
 package com.example.user;
 
-import com.example.user.vo.ResponseTemplateVO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
